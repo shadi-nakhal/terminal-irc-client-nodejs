abe00d8904c76405c5ca3cab2df5f67bcb6e3ede
ace <lebdroid@gmail.com>
2022-03-26 14:25:33 +0200

    requesting names on join fix

--- Controllers/Join.js
+++ Controllers/Join.js
@@ -1,43 +1,49 @@
-const Settings = require('../settings')
-const { Room } = require('../Components/index')
+const Settings = require("../settings");
+const { Room } = require("../Components/index");
 
+function JOIN(client, parsed) {
+    let incomingChannel = parsed.params[0].toLowerCase();
+    let identity = parsed.identity;
+    let incomingPrefix = parsed.prefix;
+    let joinedChans = Settings[identity].joinedChans;
+    let userServer = Settings[identity].server.toLowerCase();
+    let nickname = parsed.prefix.split("!")[0];
+    let usernickname = Settings[identity].nickname;
 
-function JOIN(client,parsed){
-    let incomingChannel = parsed.params[0].toLowerCase()
-    let identity = parsed.identity
-    let incomingPrefix = parsed.prefix
-    let joinedChans = Settings[identity].joinedChans
-    let userServer = Settings[identity].server.toLowerCase()
-    let nickname = parsed.prefix.split("!")[0]
-    let usernickname = Settings[identity].nickname
-
-
-    if(nickname !== usernickname){// if its not user who joined it updates the nicknames list
-     client.write(`NAMES ${incomingChannel} \r\n`)
-     if(Settings[identity][incomingChannel])
-     Settings[identity][incomingChannel].logs += "^Y**" + nickname + ` (${incomingPrefix})` + " has joined^\r\n" 
+    if (nickname !== usernickname) {
+        if (!Settings[identity][incomingChannel]) client.write(`NAMES ${incomingChannel} \r\n`);
+        if (Settings[identity][incomingChannel])
+            Settings[identity][incomingChannel].logs += "^Y**" + nickname + ` (${incomingPrefix})` + " has joined^\r\n";
+        Settings[identity][incomingChannel]["chanNicks"].push({ nickname, prefix: false });
     }
-    if(nickname === usernickname){
-        if(!joinedChans.includes(incomingChannel)){
-            Settings[identity].joinedChans = [...Settings[identity].joinedChans, incomingChannel]
-            if(!Settings[identity][incomingChannel]) Settings[identity][incomingChannel] = {name : incomingChannel, logs : '', chanNicks : [], main : '', topic : ''}
-            Settings[identity][incomingChannel].chanNicks = []
+    if (nickname === usernickname) {
+        if (!joinedChans.includes(incomingChannel)) {
+            Settings[identity].joinedChans = [...Settings[identity].joinedChans, incomingChannel];
+            if (!Settings[identity][incomingChannel])
+                Settings[identity][incomingChannel] = {
+                    name: incomingChannel,
+                    logs: "",
+                    chanNicks: [],
+                    main: "",
+                    topic: "",
+                };
+            Settings[identity][incomingChannel].chanNicks = [];
             Room.GenerateChannels({
-                type : 'channel', 
-                name : incomingChannel,
+                type: "channel",
+                name: incomingChannel,
                 server: userServer,
-                owner: identity
-            })
+                owner: identity,
+            });
         }
-    } 
+    }
 }
 
-function SetTopic(parsed){
-    let identity = parsed.identity
-    let topic = parsed.params.slice(2).join(" ")+"\r\n"
-    let channel = parsed.params[1].toLowerCase()
-    Settings[identity][channel].topic = topic
-    Settings[identity][channel].logs += topic
+function SetTopic(parsed) {
+    let identity = parsed.identity;
+    let topic = parsed.params.slice(2).join(" ") + "\r\n";
+    let channel = parsed.params[1].toLowerCase();
+    Settings[identity][channel].topic = topic;
+    Settings[identity][channel].logs += topic;
 }
 
-module.exports = { JOIN, SetTopic}
+module.exports = { JOIN, SetTopic };
