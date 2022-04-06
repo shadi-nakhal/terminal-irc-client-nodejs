47ec9053854f4b456e66044820e9a454da966035
ace <lebdroid@gmail.com>
2022-04-06 12:39:43 +0300

    buffering

--- Helpers/IrcParser.js
+++ Helpers/IrcParser.js
@@ -1,24 +1,22 @@
 function parser(data, identity) {
-  const message = {
-    raw: data,
-    command: null,
-    params: []
-  };
+    const message = {
+        raw: data,
+        command: null,
+        params: []
+    };
 
-  const splitdata = data.split(' ');
-  if (data[0] !== ':') {
-    message.command = splitdata[0];
-    message.params = splitdata[1];
+    const splitdata = data.split(" ");
+    if (data[0] !== ":") {
+        message.command = splitdata[0];
+        message.params = splitdata[1];
+        message.identity = identity;
+    }
+    message.prefix = splitdata[0].replace(":", "");
+    message.command = splitdata[1];
+    message.params = splitdata.slice(2).join(" ").replace(":", "").split(" ");
     message.identity = identity;
-  }
-  message.prefix = splitdata[0].replace(':', '');
-  message.command = splitdata[1];
-  message.params = splitdata.slice(2).join(' ').replace(':', '').split(' ');
-  message.identity = identity;
-
 
     return message;
-  
 }
 
 module.exports = parser;
