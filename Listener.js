9eaef036301f1e740e2221121bbf8016f059c9be
ace <lebdroid@gmail.com>
2022-04-10 13:14:45 +0300

    server tab/bugger

--- Listener.js
+++ Listener.js
@@ -40,81 +40,89 @@ class Listener extends Connecting {
         let temp = "";
         client.on("data", (data) => {
             data = data.toString();
-            let len = data.length - 1 ;
+            let len = data.length - 1;
             buffer(data);
-            function buffer(data){
-
-              if(data[len] !== '\n'){
-                temp = data;
-                return;
-              }
-
-              if(data[len] === '\n'){
-                if(temp.length > 0){
-                  message.push(temp + data);
-                  temp = "";
-                  return;
-                }else{
-                  if(data.length !== 0)
-                    message.push(data);
+            function buffer(data) {
+                if (data[len] !== "\n") {
+                    temp = data;
+                    return;
                 }
+
+                if (data[len] === "\n") {
+                    if (temp.length > 0) {
+                        message.push(temp + data);
+                        temp = "";
+                        return;
+                    } else {
+                        if (data.length > 0) {
+                            message.push(data);
+                        }
+                    }
                 }
-          }
+            }
             const rnparse = message.flatMap((e) => e.split("\r\n"));
             // const rnparse = data.split("\r\n");
-            console.logger(rnparse);
             for (const raw of rnparse) {
-                const date = `${new Date().toTimeString().slice(0, 8)}`;
-                const parsed = IrcParser(raw, identity);
-                const { command, params } = parsed;
-                // Settings[identity].status += `${date}: ${params.slice(1).join(" ")}\r\n`;
-                Settings[identity].status += `${date}: ${params.slice(1).join(" ")}\r\n`;
-                // console.logger(`${date}: ${params.slice(1).join(" ")}\r\n`);
-                if (command === "NOTICE") Notice(parsed);
-                if (command === "PING" || command === "PONG") PING(parsed, params, client);
-                if (command === "001") WELCOME(params, identity, client); // RPL_WELCOME (001)
-                if (command === "462") AlreadyReg(parsed, client);
-                if (command === "004") SERVERINFO(identity, server); // RPL_MYINFO (004) server info
-                if (command === "376" || command === "422") MOTD(client, parsed); // RPL_MOTD (372)
-                if (command === "353") GETNAMES(parsed); // RPL_NAMREPLY (353
-                if (command === "366") SHOWNAMES(parsed); // RPL_ENDOFNAMES (366)'
-                if (command === "NICK") CHANGEDNICK(parsed, identity);
-                if (command === "JOIN") JOIN(client, parsed);
-                if (command === "PART" || command === "QUIT" || command === "KICK") LEAVING(parsed, identity);
-                if (command === "311") WHOISUSER(params);
-                if (command === "312") WHOISSERVER(params, client);
-                if (command === "319") WHOISCHANNELS(params, client);
-                if (command === "317") WHOISIDLE(params, client);
-                if (command === "318") ENDOFWHOIS(parsed, client);
-                if (command === "332") SetTopic(parsed);
-                if (command === "PRIVMSG") PRIVMSG(parsed, client);
-                if (command === "433") NICKNAMEINUSE(client, parsed, identity); // ERR_NICKNAMEINUSE (433)
-                if (command === "MODE") MODE(parsed);
-                if (command === "ERROR") HandleKill(client, raw);
-                message.pop();
+                if (raw.length > 0) {
+                    const date = `${new Date().toTimeString().slice(0, 8)}`;
+                    const parsed = IrcParser(raw, identity);
+                    const { command, params } = parsed;
 
+                    if (
+                        !(
+                            command === "PING" ||
+                            command === "NOTICE" ||
+                            command === "PONG" ||
+                            command === "NICK" ||
+                            command === "JOIN" ||
+                            command === "NAMES" ||
+                            command === "PART" ||
+                            command === "QUIT" ||
+                            command === "KICK" ||
+                            command === "311" ||
+                            command === "333" ||
+                            command === "312" ||
+                            command === "319" ||
+                            command === "318" ||
+                            command === "332" ||
+                            command === "366" ||
+                            command === "353" ||
+                            command === "MODE" ||
+                            command === "PRIVMSG"
+                        )
+                    ) {
+                        Settings[identity].status += `${date}: ${params.slice(1).join(" ")}\r\n`;
+                        console.logger(`${date}: ${params.slice(1).join(" ")}\r\n`);
+                        // Settings[identity].status += `${date}: ${raw}\r\n`;
+                    }
+                    // Settings[identity].status += `${date}: ${raw}\r\n`;
+
+                    if (command === "NOTICE") Notice(parsed);
+                    if (command === "PING" || command === "PONG") PING(parsed, params, client);
+                    if (command === "001") WELCOME(params, identity, client); // RPL_WELCOME (001)
+                    if (command === "462") AlreadyReg(parsed, client);
+                    if (command === "004") SERVERINFO(identity, server); // RPL_MYINFO (004) server info
+                    if (command === "376" || command === "422") MOTD(client, parsed); // RPL_MOTD (372)
+                    if (command === "353") GETNAMES(parsed); // RPL_NAMREPLY (353
+                    if (command === "366") SHOWNAMES(parsed); // RPL_ENDOFNAMES (366)'
+                    if (command === "NICK") CHANGEDNICK(parsed, identity);
+                    if (command === "JOIN") JOIN(client, parsed);
+                    if (command === "PART" || command === "QUIT" || command === "KICK") LEAVING(parsed, identity);
+                    if (command === "311") WHOISUSER(params);
+                    if (command === "312") WHOISSERVER(params, client);
+                    if (command === "319") WHOISCHANNELS(params, client);
+                    if (command === "317") WHOISIDLE(params, client);
+                    if (command === "318") ENDOFWHOIS(parsed, client);
+                    if (command === "332") SetTopic(parsed);
+                    if (command === "PRIVMSG") PRIVMSG(parsed, client);
+                    if (command === "433") NICKNAMEINUSE(client, parsed, identity); // ERR_NICKNAMEINUSE (433)
+                    if (command === "MODE") MODE(parsed);
+                    if (command === "ERROR") HandleKill(client, raw);
+                    message.pop();
+                }
             }
-          });
+        });
     }
-
-    // bufferStack(data){
-    //   data = data.toString();
-    //   let len = data.length - 1 ;
-    //   let message;
-    //   let temp;
-    //   if(data[len] === '\n'){
-    //     if(temp.length > 0){
-    //       message.push(...temp);
-    //       temp = "";
-    //       return;
-    //     }
-    //   }
-    //   if(data[len - 1] !== '\r'){
-    //     temp += data;
-    //   }
-    //   message.push(data);
-    //   return;
-    // }
 }
 
 module.exports = { Listener };
